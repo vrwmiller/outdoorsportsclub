f844936dcdc04a7da1adbbba6e7fcd6dc276d31b
Vincent Miller <vrwmiller@gmail.com>
2026-03-26 22:11:17 -0400

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import ConfigureAmplify from "@/components/ConfigureAmplify";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Outdoor Sports Club",
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className="bg-gray-50 min-h-screen text-gray-800 text-base">
         <ConfigureAmplify />
         {children}
